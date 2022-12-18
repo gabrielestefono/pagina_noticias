2e9018f52bd62bdd4d8413558568aae8ed606a79
Gabriel Estéfono <gabrielestefono@hotmail.com>
2022-12-18 08:47:18 -0300

    Oitavo Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
-Menu = document.getElementById('menu')
+const Menu = document.getElementById('menu')
+const show = document.getElementById('show')
 
 Menu.addEventListener('click', ()=>{
     if (Menu.classList == "header__hamburger"){
         Menu.classList = 'header__hamburger--close'
+        show.classList = 'sidemenu--show'
     }else{
         Menu.classList = "header__hamburger"
+        show.classList = 'sidemenu--hide'
     }
-})
+});
---
+++ assets/js/sidemenu.js
@@ -0,0 +1,82 @@
+// # Selecionar corpo
+const corpo = document.body;
+
+// ## Seleção da tag header
+const header = document.getElementsByTagName('header');
+
+
+// CRIAR ELEMENTOS NECESSÁRIOS
+
+// # DIV filha do Body - primária
+const criarDivPrincipal = document.createElement('div');
+
+// ## DIV Secundária
+const criarDivSecundaria = document.createElement('div');
+
+// ### Lista Não Ordenada, filha da DIV secundária
+const criarListaNaoOrdenada = document.createElement('ul');
+
+// #### Itens da lista não ordenada
+const criarItemHome = document.createElement('li');
+const criarItemNew = document.createElement('li');
+const criarItemPopular = document.createElement('li');
+const criarItemTrending = document.createElement('li');
+const criarItemCategories = document.createElement('li');
+
+// ##### Links dos itens da lista
+const criarLinkItemHome = document.createElement('a');
+const criarLinkItemNew = document.createElement('a');
+const criarLinkItemPopular = document.createElement('a');
+const criarLinkItemTrending = document.createElement('a');
+const criarLinkItemCategories = document.createElement('a');
+
+
+// INSERIR OS ITENS ONDE DEVEM SER INSERIDOS
+
+
+// # Inserindo DIV primária
+corpo.insertBefore(criarDivPrincipal, header[0]);
+
+// ## Inserindo DIV secundária como filha da DIV primária
+criarDivPrincipal.appendChild(criarDivSecundaria);
+
+// ### Inserindo lista não ordenada na DIV secundária
+criarDivSecundaria.appendChild(criarListaNaoOrdenada);
+
+// #### Inserindo itens na lista não ordenada
+criarListaNaoOrdenada.appendChild(criarItemHome);
+criarListaNaoOrdenada.appendChild(criarItemNew);
+criarListaNaoOrdenada.appendChild(criarItemPopular);
+criarListaNaoOrdenada.appendChild(criarItemTrending);
+criarListaNaoOrdenada.appendChild(criarItemCategories);
+
+// ##### Inserindo tags de links aos itens da lista
+criarItemHome.appendChild(criarLinkItemHome);
+criarItemNew.appendChild(criarLinkItemNew);
+criarItemPopular.appendChild(criarLinkItemPopular);
+criarItemTrending.appendChild(criarLinkItemTrending);
+criarItemCategories.appendChild(criarLinkItemCategories);
+
+
+// INSERINDO TEXTOS AOS LINKS
+
+
+criarLinkItemHome.innerText = 'Home'
+criarLinkItemNew.innerText = 'New'
+criarLinkItemPopular.innerText = 'Popular'
+criarLinkItemTrending.innerText = 'Trending'
+criarLinkItemCategories.innerText = 'Categories'
+
+
+// ADICIONAR CLASSES E ID'S ÀS TAG'S
+
+
+// # DIV Primária
+criarDivPrincipal.classList = 'sidemenu--hide';
+criarDivPrincipal.id = 'show';
+
+// ## DIV Secundária
+
+
+
+//ADDEVENTLISTERNER FICOU SEPARADO NO ARQUIVO SCRIPT.JS
